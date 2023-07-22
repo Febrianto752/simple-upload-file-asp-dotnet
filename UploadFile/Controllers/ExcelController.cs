a04c9287c4a6a46ad9fbe5100fb8ae72cf2dc6d7
Febrianto752 <febrianto.bekasi@gmail.com>
2023-07-22 15:15:13 +0700

    create import excel with closed xml part2

--- UploadFile/Controllers/ExcelController.cs
+++ UploadFile/Controllers/ExcelController.cs
@@ -8,69 +8,39 @@ public class ExcelController : Controller
 {
     public IActionResult Index()
     {
-        var products = ImportExcel<Product>(@"C:\Users\Febrianto\Desktop\latihan-upload-file\UploadFile\UploadFile\product-test.xlsx", "Products");
-        Console.WriteLine(products);
+        var products = ImportExcel(@"C:\Users\Febrianto\Desktop\latihan-upload-file\UploadFile\UploadFile\product-test.xlsx", "Products");
+        foreach (var product in products)
+        {
+            Console.WriteLine($"name : {product.Name}");
+            Console.WriteLine($"price : {product.Price}");
+            Console.WriteLine($"units : {product.Units}");
+            Console.WriteLine();
+        }
         return View();
     }
 
 
 
-    public List<T> ImportExcel<T>(string excelFilePath, string sheetName)
+    public List<Product> ImportExcel(string excelFilePath, string sheetName)
     {
-        var list = new List<T>();
+        var list = new List<Product>();
 
-        Type typeOfObject = typeof(T);
-
-        using (IXLWorkbook workbook = new XLWorkbook(excelFilePath))
+        using (var workbook = new XLWorkbook(excelFilePath))
         {
-            var worksheet = workbook.Worksheets.Where(w => { Console.WriteLine(w.Name); return w.Name == sheetName; }).FirstOrDefault();
-            var properties = typeOfObject.GetProperties();
-            Console.WriteLine($"properties : {properties}");
-            // header column texts
-            var columns = worksheet.FirstRow().Cells().Select((v, i) => new { Value = v.Value, Index = i + 1 });
+            var worksheet = workbook.Worksheet(1); // Ambil worksheet pertama
 
-            // skip first row which is used for column header texts
-            foreach (IXLRow row in worksheet.RowsUsed().Skip(1))
+            var rows = worksheet.RowsUsed();
+
+            foreach (var row in rows.Skip(1)) // Lewati baris pertama karena itu adalah judul kolom
             {
-                T obj = (T)Activator.CreateInstance(typeOfObject);
-                Console.WriteLine($"obj : {obj}");
-                foreach (var prop in properties)
-                {
-                    int colIndex = columns.SingleOrDefault(c => c.Value.ToString().ToLower() == prop.Name.ToString().ToLower()).Index;
-                    var val = row.Cell(colIndex).Value;
-                    var type = prop.PropertyType;
-                    Console.WriteLine($"val : {val.GetType()}");
-                    Console.WriteLine($"type : {type}");
-                    Console.WriteLine($"prop : {prop.GetType()}");
-                    Console.WriteLine();
-                    prop.SetValue(obj, val);
+                var product = new Product();
+                product.Name = row.Cell(1).Value.ToString();
+                product.Price = Convert.ToDecimal(row.Cell(2).Value.ToString());
+                product.Units = Convert.ToInt32(row.Cell(3).Value.ToString());
 
-                }
-
-                list.Add(obj);
+                list.Add(product);
             }
         }
-
-
-        //using (var workbook = new XLWorkbook(excelFilePath))
-        //{
-        //    var worksheet = workbook.Worksheet(1); // Ambil worksheet pertama
-
-        //    var rows = worksheet.RowsUsed();
-
-        //    foreach (var row in rows.Skip(1)) // Lewati baris pertama karena itu adalah judul kolom
-        //    {
-        //        Product product = new Product();
-        //        product.Name = row.Cell(1).Value.ToString();
-        //        product.Price = Convert.ToDecimal(row.Cell(2).Value);
-        //        product.Units = Convert.ToInt32(row.Cell(3).Value);
-
-        //        list.Add(product);
-        //    }
-        //}
-
-
-
 
         return list;
     }
